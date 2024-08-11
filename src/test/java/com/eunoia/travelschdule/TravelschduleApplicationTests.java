2d66e054a5d0b6470628bd6c83cb8df15b3cd04b
youngdon <eunoia3802@gmail.com>
2024-08-11 21:43:08 +0900

    initial commit

---
+++ src/test/java/com/eunoia/travelschdule/TravelschduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eunoia.travelschdule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelschduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
