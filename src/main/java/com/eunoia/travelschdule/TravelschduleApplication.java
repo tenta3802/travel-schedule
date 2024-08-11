365bc39b3a9f3859926a124e0effc0ff8117008b
youngdon <eunoia3802@gmail.com>
2024-08-11 21:43:08 +0900

    initial commit

---
+++ src/main/java/com/eunoia/travelschdule/TravelschduleApplication.java
@@ -0,0 +1,13 @@
+package com.eunoia.travelschdule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelschduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelschduleApplication.class, args);
+	}
+
+}
