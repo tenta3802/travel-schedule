7453a071d6bd67540cf3f5aa06547aea097c0866
youngdon <eunoia3802@gmail.com>
2024-10-09 14:05:40 +0900

    feat: JwtAuthenticationFilter 구현
    jwt에서 추출한 사용자 정보 SecurityContextHolder에 저장되도록 구현

---
+++ src/main/java/com/eunoia/travelschdule/global/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,57 @@
+package com.eunoia.travelschdule.global.filter;
+
+import com.eunoia.travelschdule.domain.token.application.TokenProvider;
+import io.jsonwebtoken.Claims;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final TokenProvider tokenProvider;
+    private final UserDetailsService userDetailsService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        String jwt = getJwtFromRequest(request);
+
+        if (jwt != null && tokenProvider.validateToken(jwt)) {
+            Claims claims = tokenProvider.getAllClaimsFromToken(jwt);
+            String username = claims.getSubject();
+            String roles = claims.get("roles").toString();
+
+            UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+
+            if (userDetails != null) {
+                UsernamePasswordAuthenticationToken authentication =
+                        new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String getJwtFromRequest(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+        if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+}
